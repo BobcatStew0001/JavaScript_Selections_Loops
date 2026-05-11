2baf94a8d6ac2826b2c621cafed7f698d4678082
Zach Brown <bigcountry66789@gmail.com>
2026-05-11 17:49:50 -0500

    Completed all of the exercises

--- app.js
+++ app.js
@@ -5,3 +5,13 @@ console.log("EXERCISE 1:\n==========\n");
 
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
+
+//Exercise 1 
+for(let oddNumber = 1; oddNumber <= 100; oddNumber++)
+{
+if(oddNumber % 2 != 0)
+    {
+    console.log(oddNumber)
+    }
+
+}
